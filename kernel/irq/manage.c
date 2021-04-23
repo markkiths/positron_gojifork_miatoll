41ad3e416e93c5c4e450a1c18ad831133aa42ccc
Vaisakh Murali <mvaisakh@statixos.com>
2021-04-23 22:25:09 +0530

    kernel: irq: Switch to bi-cluster API for marking IRQ threads as critical
    * surya has a bi-cluster setup.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -27,7 +27,6 @@
 static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
-static int prime_cpu_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1216,8 +1215,6 @@ static void affine_one_perf_thread(struct irqaction *action)
 
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
-	else
-		mask = cpu_prime_mask;
 
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
@@ -1241,9 +1238,6 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
-	} else {
-		mask = cpu_prime_mask;
-		mask_index = &prime_cpu_index;
 	}
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
@@ -1317,7 +1311,6 @@ void reaffine_perf_irqs(bool from_hotplug)
 	if (!from_hotplug || !perf_crit_suspended) {
 		perf_crit_suspended = false;
 		perf_cpu_index = -1;
-		prime_cpu_index = -1;
 		list_for_each_entry(desc, &perf_crit_irqs, perf_list) {
 			raw_spin_lock(&desc->lock);
 			affine_one_perf_irq(desc, desc->action->flags);
@@ -1585,7 +1578,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;
